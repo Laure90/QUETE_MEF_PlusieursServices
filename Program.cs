67eaf6dd0d0103ec3b05323bb77ee50e054691fb
Laure90 <58706312+Laure90@users.noreply.github.com>
2020-04-06 15:38:23 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.Composition;
+using System.ComponentModel.Composition.Hosting;
+using System.IO;
+
+namespace QUETE_MEF_PlusieursServices
+{
+    public class Program
+    {
+        [ImportMany(typeof(ILogger))]
+        private Logger _logger;
+
+        static void Main(string[] args)
+        {
+            Host host = new Host();
+            host.Run();
+
+            Console.Read();
+        } 
+    }
+
+    internal class Host
+    {
+
+        [ImportMany(typeof(ILogger))]
+        protected IEnumerable<ILogger> _logger = null;
+
+        public void Run()
+        {
+            var container = new CompositionContainer();
+            container.ComposeParts(this, new Logger(), new LoggerTxt());
+            foreach (var logger in _logger)
+            {
+                logger.Write(" World !");
+            }
+        }
+    }
+
+    [Export(typeof(ILogger))]
+    internal class LoggerTxt : ILogger
+    {
+        public void Write(string message)
+        {
+            TextWriter writeFile = new StreamWriter("textwriter.txt");
+            writeFile.WriteLine("Hello " + message + " this is a file text !");
+            writeFile.Flush();
+            writeFile.Close();
+
+        }
+    }
+
+    [Export(typeof(ILogger))]
+    internal class Logger : ILogger
+    {
+        public void Write(string message)
+        {
+            Console.WriteLine("Hello" + message);
+        }
+    }
+}
